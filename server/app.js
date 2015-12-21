8f24f31f5a79fc9816927769395ab76271d385d1
Ivan Demchenko <raqy.style@gmail.com>
2015-12-21 19:02:08 +0100

    first commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = require('./router');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+router(app);
+
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.static(path.join(__dirname, '../public')));
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
