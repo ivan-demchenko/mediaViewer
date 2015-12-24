677dca1803f3da35434deb2ce3839b582a3b223d
Ivan Demchenko <raqy.style@gmail.com>
2015-12-24 14:56:36 +0100

    Added ESC key support

--- client/src/streams.js
+++ client/src/streams.js
@@ -5,6 +5,11 @@ import S from './service'
 const currPathBus = new Bacon.Bus();
 const imageToPreview = new Bacon.Bus();
 
+const globalKeyUp = Bacon.fromEvent(window.document.body, 'keyup').map('.which');
+const escKey = globalKeyUp.filter(27);
+
+imageToPreview.plug(escKey.map(null));
+
 export default {
   listing: currPathBus.flatMap(R.compose(Bacon.fromPromise, S.getPath)),
   currentPath: currPathBus,
